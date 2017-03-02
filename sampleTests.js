e9c4f20678cdaf862f910799b4f5547c25f7ba1e
Blake DeBoer <rudyoyen@users.noreply.github.com>
2017-03-02 10:46:45 -0500

    add instructions for tests

--- sampleTests.js
+++ sampleTests.js
@@ -1,3 +1,6 @@
+// To run these tests, execute in the console: node sampleTests.js
+// To see more readable results use: node sampleTests.js | faucet or node sampleTests.js | tap-dot
+
 var test = require('tape');
 
 test('first sample', function(t) {
--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-//COPIED from Eric Elliott's blog post: https://medium.com/javascript-scene/why-i-use-tape-instead-of-mocha-so-should-you-6aa105d8eaf4#.me1nfq160
+//	COPIED from Eric Elliott's blog post: 
+//	https://medium.com/javascript-scene/why-i-use-tape-instead-of-mocha-so-should-you-6aa105d8eaf4#.me1nfq160
+
+/*
+To run these tests type into your console: babel-node test.js
+To see more readable results use: babel-node test.js | faucet or babel-node test.js | tap-dot 
+
+To run it in a browser, run the command:
+browserify -t babelify test.js | browser-run -p 2222 | faucet
+And then go to: http://localhost:2222
+*/
 
 import test from 'tape';
 
